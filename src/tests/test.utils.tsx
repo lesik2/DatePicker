e67edafa1d9c2d06b62c94f4b1d5b0e00db2c5d9
Aliaksei Pukhalski <lesikpuhalskij@gmail.com>
2023-12-18 19:02:51 +0300

    fix: update test.utils.tsx

--- src/tests/test.utils.tsx
+++ src/tests/test.utils.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React, {ReactElement} from 'react'
-import {render, RenderOptions, RenderResult} from '@testing-library/react'
+import {render, RenderOptions} from '@testing-library/react'
 import {ThemeProvider} from 'styled-components'
 
 import {theme} from '../constants/theme'
@@ -14,8 +14,8 @@ const AllTheProviders = ({children}: {children: React.ReactNode}) => (
 const customRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, 'wrapper'>,
-): RenderResult<typeof import("d:/DatePicker/DatePicker/node_modules/@testing-library/dom/types/queries"), HTMLElement, HTMLElement> => 
-render(ui, {wrapper: AllTheProviders, ...options})
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+)=> render(ui, {wrapper: AllTheProviders, ...options})
 
 export * from '@testing-library/react'
 export {customRender as render}
