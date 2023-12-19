1684319b793ab4c1b2c07e627c4a794094ed0add
Aliaksei Pukhalski <lesikpuhalskij@gmail.com>
2023-12-19 21:45:44 +0300

    fix: update useShowTooltip hook

--- src/components/Tooltip/hooks/useShowTooltip.tsx
+++ src/components/Tooltip/hooks/useShowTooltip.tsx
@@ -5,10 +5,10 @@ export function useShowTooltip(
   isOpen: boolean, setIsOpen: Dispatch<React.SetStateAction<boolean>>
   ): void{
   useEffect(()=>{
-    if(!isOpen) return;
+    if(!isOpen) return ()=>{};
+    
     const idTimer = setTimeout(()=> setIsOpen(false), 1800)
 
-    // eslint-disable-next-line consistent-return
     return ()=>{
       clearTimeout(idTimer);
     }
